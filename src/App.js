3ebd7458edb6b3dbb26251afe20deb48bc5d98e5
Pooja <shettypooja2000@gmail.com>
2021-03-08 12:08:02 +0530

    understanding props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Persons/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello Iam React App</h1>
+        <Person name="Pooja" age="21"/>
+        <Person name="Pallavi" age="20">Hobbies: Singing</Person>
+          <Person name="Rajani" age="21"></Person>
       </div>
     );
   }
---
+++ src/Persons/Person.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const Person=(props)=>{
+    return(
+        <div>
+            <h1>Hello Iam {props.name} and I am {props.age} years old</h1>
+            <p>{props.children}</p>                                                                     {/*To get other elements of the tag*/}
+        </div>
+
+    )
+}
+
+export default Person
