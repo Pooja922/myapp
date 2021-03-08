b2e14176ff963f4f41a5086a98402072d3d56067
Pooja <shettypooja2000@gmail.com>
2021-03-08 14:48:30 +0530

    passing method references between components

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ class App extends Component {
             {name:"Rajani",age:21}
             ]
     }
-    switchNameHandler=()=>{
+    switchNameHandler=(newName)=>{
         this.setState({
             persons:[
-                {name:"Pooja A Shetty",age:21},
+                {name:newName,age:21},
                 {name:"Pallavi",age:20},
                 {name:"Rajani",age:22}
             ]
@@ -27,9 +27,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hello Iam React App</h1>
-          <button onClick={this.switchNameHandler}>Switch Name</button>
+          <button onClick={()=>this.switchNameHandler.bind(this,"Pooja A Shetty")}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: Singing</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this,"Pooja A Shetty!")}>Hobbies: Singing</Person>
           <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
--- src/Persons/Person.js
+++ src/Persons/Person.js
@@ -2,7 +2,7 @@ import React from 'react';
 const Person=(props)=>{
     return(
         <div>
-            <h1>Hello Iam {props.name} and I am {props.age} years old</h1>
+            <h1 onClick={props.click}>Hello Iam {props.name} and I am {props.age} years old</h1>
             <p>{props.children}</p>                                                                     {/*To get other elements of the tag*/}
         </div>
 
