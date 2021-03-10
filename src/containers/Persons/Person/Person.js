3fa750fc4e5ead355631f248a8b888f194419f5e
Pooja <shettypooja2000@gmail.com>
2021-03-10 10:05:38 +0530

    Minor modification in components directory

---
+++ src/containers/Persons/Person/Person.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Person.css';
+//import styled from "styled-components"
+//import Radium from "radium";
+/*const StyledDiv=styled.div`
+  width:60%;
+  margin: 16px auto;
+  border:1px solid #eee;
+  box-shadow: 0 2px 3px #ccc;
+  padding:16px;
+  text-align:center;
+
+  @media (min-width:500px){
+  width:450px;
+};
+`*/
+const Person=(props)=>{
+    /* const style={
+         '@media (minWidth:500px)':{
+             width:'450px',
+     }
+     }*/
+    const rnd =Math.random();
+    if (rnd>0.7){
+        throw new Error('Something went wrong')
+    }
+    return(
+
+        <div className="Person" /*style={style}*/>
+            {/*<StyledDiv>*/}
+            <h1 onClick={props.click}>Hello Iam {props.name} and I am {props.age} years old</h1>
+            <p>{props.children}</p>
+            {/*To get other elements of the tag*/}
+            <input type="text" onChange={props.changes} value={props.name}/>
+            {/*</StyledDiv>*/}
+        </div>
+    )
+}
+
+/*
+export default Radium(Person)*/
+export default Person
